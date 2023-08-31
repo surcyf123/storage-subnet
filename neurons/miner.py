300221a333378abbb6f9da619bfef6398d69aef3
EC2 Default User <ec2-user@ip-172-31-46-82.ca-central-1.compute.internal>
2023-08-31 09:01:11 +0000

    initial rewrite of neurons

--- neurons/miner.py
+++ neurons/miner.py
@@ -1,61 +1,18 @@
-# The MIT License (MIT)
-# Copyright © 2023 Yuma Rao
-# TODO(developer): Set your name
-# Copyright © 2023 <your name>
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
-# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
-# the Software.
-
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-
-# Bittensor Miner Template:
-# TODO(developer): Rewrite based on protocol and validator defintion.
-
-# Step 1: Import necessary libraries and modules
 import os
 import time
 import argparse
-import traceback
+import rocksdb
 import bittensor as bt
 
-# Custom modules
-import rocksdb
-              
-# import this repo
-import storage
-
 def get_config():
-    # Step 2: Set up the configuration parser
-    # This function initializes the necessary command-line arguments.
-    # Using command-line arguments allows users to customize various miner settings.
     parser = argparse.ArgumentParser()
-    # TODO(developer): Adds your custom miner arguments to the parser.
-    parser.add_argument('--path_to_data', default='~/data_db', help='Miner DB location generated from cargo run -- --path ~/datadb --n 10 --size 10000000 --seed 0 --hash')
-    # Adds override arguments for network and netuid.
-    parser.add_argument( '--netuid', type = int, default = 1, help = "The chain subnet uid." )
-    # Adds subtensor specific arguments i.e. --subtensor.chain_endpoint ... --subtensor.network ...
+    parser.add_argument('--path_to_data', default='~/data_db', help='Data storage location for the miner.')
+    parser.add_argument('--netuid', type=int, default=1, help="The chain subnet uid.")
     bt.subtensor.add_args(parser)
-    # Adds logging specific arguments i.e. --logging.debug ..., --logging.trace .. or --logging.logging_dir ...
     bt.logging.add_args(parser)
-    # Adds wallet specific arguments i.e. --wallet.name ..., --wallet.hotkey ./. or --wallet.path ...
     bt.wallet.add_args(parser)
-    # Adds axon specific arguments i.e. --axon.port ...
     bt.axon.add_args(parser)
-    # Activating the parser to read any command-line inputs.
-    # To print help message, run python3 template/miner.py --help
     config = bt.config(parser)
-
-    # Step 3: Set up logging directory
-    # Logging captures events for diagnosis or understanding miner's behavior.
     config.full_path = os.path.expanduser(
         "{}/{}/{}/netuid{}/{}".format(
             config.logging.logging_dir,
@@ -65,159 +22,69 @@ def get_config():
             'miner',
         )
     )
-    # Ensure the directory for logging exists, else create one.
     if not os.path.exists(config.full_path): os.makedirs(config.full_path, exist_ok=True)
     return config
 
-
-# Main takes the config and starts the miner.
-def main( config ):
-
-    # Activating Bittensor's logging with the set configurations.
+def main(config):
     bt.logging(config=config, logging_dir=config.full_path)
     bt.logging.info(f"Running miner for subnet: {config.netuid} on network: {config.subtensor.chain_endpoint} with config:")
-
-    # This logs the active configuration to the specified logging directory for review.
     bt.logging.info(config)
 
-    # Step 4: Initialize Bittensor miner objects
-    # These classes are vital to interact and function within the Bittensor network.
-    bt.logging.info("Setting up bittensor objects.")
-
-    # Wallet holds cryptographic information, ensuring secure transactions and communication.
-    wallet = bt.wallet( config = config )
-    bt.logging.info(f"Wallet: {wallet}")
-
-    # subtensor manages the blockchain connection, facilitating interaction with the Bittensor blockchain.
-    subtensor = bt.subtensor( config = config )
-    bt.logging.info(f"Subtensor: {subtensor}")
-
-    # metagraph provides the network's current state, holding state about other participants in a subnet.
+    wallet = bt.wallet(config=config)
+    subtensor = bt.subtensor(config=config)
     metagraph = subtensor.metagraph(config.netuid)
-    bt.logging.info(f"Metagraph: {metagraph}")
 
     if wallet.hotkey.ss58_address not in metagraph.hotkeys:
-        bt.logging.error(f"\nYour validator: {wallet} if not registered to chain connection: {subtensor} \nRun btcli register and try again. ")
+        bt.logging.error(f"Your miner is not registered. Run btcli register and try again.")
         exit()
     else:
-        # Each miner gets a unique identity (UID) in the network for differentiation.
         my_subnet_uid = metagraph.hotkeys.index(wallet.hotkey.ss58_address)
         bt.logging.info(f"Running miner on uid: {my_subnet_uid}")
 
-    # Build my DB
-    data_db = rocksdb.DB(data_path, rocksdb.Options(create_if_missing=True))
+    data_db = rocksdb.DB(config.path_to_data, rocksdb.Options(create_if_missing=True))
 
-    # Step 4: Set up miner functionalities
-    # The following functions control the miner's response to incoming requests.
-    # The blacklist function decides if a request should be ignored.
-    def blacklist_fn( synapse: storage.protocol.Dummy ) -> bool:
-        # TODO(developer): Define how miners should blacklist requests. This Function 
-        # Runs before the synapse data has been deserialized (i.e. before synapse.data is available).
-        # The synapse is instead contructed via the headers of the request. It is important to blacklist
-        # requests before they are deserialized to avoid wasting resources on requests that will be ignored.
-        # Below: Check that the hotkey is a registered entity in the metagraph.
-        if synapse.dendrite.hotkey not in metagraph.hotkeys:
-            # Ignore requests from unrecognized entities.
-            bt.logging.trace(f'Blacklisting unrecognized hotkey {synapse.dendrite.hotkey}')
-            return True
-        # TODO(developer): In practice it would be wise to blacklist requests from entities that 
-        # are not validators, or do not have enough stake. This can be checked via metagraph.S
-        # and metagraph.validator_permit. You can always attain the uid of the sender via a
-        # metagraph.hotkeys.index( synapse.dendrite.hotkey ) call.
-        # Otherwise, allow the request to be processed further.
-        bt.logging.trace(f'Not Blacklisting recognized hotkey {synapse.dendrite.hotkey}')
-        return False
-
-    # The priority function determines the order in which requests are handled.
-    # More valuable or higher-priority requests are processed before others.
-    def priority_fn( synapse: storage.protocol.Dummy ) -> float:
-        # TODO(developer): Define how miners should prioritize requests.
-        # Miners may recieve messages from multiple entities at once. This function
-        # determines which request should be processed first. Higher values indicate
-        # that the request should be processed first. Lower values indicate that the
-        # request should be processed later.
-        # Below: simple logic, prioritize requests from entities with more stake.
-        caller_uid = metagraph.hotkeys.index( synapse.dendrite.hotkey ) # Get the caller index.
-        prirority = float( metagraph.S[ caller_uid ] ) # Return the stake as the priority.
-        bt.logging.trace(f'Prioritizing {synapse.dendrite.hotkey} with value: ', prirority)
-        return prirority
-    
-    # This is the core miner function, which decides the miner's response to a valid, high-priority request.
-    def store( synapse: storage.protocol.Store ) -> storage.protocol.Store:
-        # TODO(developer): Define how miners should process requests.
-        # This function runs after the synapse has been deserialized (i.e. after synapse.data is available).
-        # This function runs after the blacklist and priority functions have been called.
-        # Below: simple template logic: return the input value multiplied by 2.
-        # If you change this, your miner will lose emission in the network incentive landscape.
-        DB[ synapse.key ] = synapse.data
-        return synapse
-    
-    # This is the core miner function, which decides the miner's response to a valid, high-priority request.
-    def retrieve( synapse: storage.protocol.Retrieve ) -> storage.protocol.Retrieve:
-        # Returns the data stored at the key.
-        synapse.data = data_db.get(rocksdb.ReadOptions(), data_key)
+    def store(synapse):
+        data_key = synapse.key.encode()
+        data_value = synapse.data.encode()
+        data_db.put(data_key, data_value)
         return synapse
 
-    # Step 5: Build and link miner functions to the axon.
-    # The axon handles request processing, allowing validators to send this process requests.
-    axon = bt.axon( wallet = wallet )
-    bt.logging.info(f"Axon {axon}")
+    def retrieve(synapse):
+        data_key = synapse.key.encode()
+        synapse.data = data_db.get(data_key).decode()
+        return synapse
 
-    # Attach determiners which functions are called when servicing a request.
-    bt.logging.info(f"Attaching forward function to axon.")
+    axon = bt.axon(wallet=wallet)
+
     axon.attach(
-        forward_fn = store,
-        blacklist_fn = blacklist_fn,
-        priority_fn = priority_fn,
+        forward_fn=store,
     ).attach(
-        forward_fn = retrieve,
-        blacklist_fn = blacklist_fn,
-        priority_fn = priority_fn,
+        forward_fn=retrieve,
     )
 
-    # Serve passes the axon information to the network + netuid we are hosting on.
-    # This will auto-update if the axon port of external ip have changed.
-    bt.logging.info(f"Serving axon {store} and {retrieve} on network: {config.subtensor.chain_endpoint} with netuid: {config.netuid}")
-    axon.serve( netuid = config.netuid, subtensor = subtensor )
+    bt.logging.info(f"Serving axon on network: {config.subtensor.chain_endpoint} with netuid: {config.netuid}")
+    axon.serve(netuid=config.netuid, subtensor=subtensor)
 
-    # Start  starts the miner's axon, making it active on the network.
     bt.logging.info(f"Starting axon server on port: {config.axon.port}")
     axon.start()
 
-    # Step 6: Keep the miner alive
-    # This loop maintains the miner's operations until intentionally stopped.
     bt.logging.info(f"Starting main loop")
     step = 0
     while True:
         try:
-            # TODO(developer): Define any additional operations to be performed by the miner.
-            # Below: Periodically update our knowledge of the network graph.
             if step % 5 == 0:
                 metagraph = subtensor.metagraph(config.netuid)
-                log =  (f'Step:{step} | '\
-                        f'Block:{metagraph.block.item()} | '\
-                        f'Stake:{metagraph.S[my_subnet_uid]} | '\
-                        f'Rank:{metagraph.R[my_subnet_uid]} | '\
-                        f'Trust:{metagraph.T[my_subnet_uid]} | '\
-                        f'Consensus:{metagraph.C[my_subnet_uid] } | '\
-                        f'Incentive:{metagraph.I[my_subnet_uid]} | '\
-                        f'Emission:{metagraph.E[my_subnet_uid]}')
+                log =  (f'Step:{step} | '
+                        f'Block:{metagraph.block.item()} | '
+                        f'Stake:{metagraph.S[my_subnet_uid]} | '
+                        f'Rank:{metagraph.R[my_subnet_uid]}')
                 bt.logging.info(log)
             step += 1
             time.sleep(1)
-
-        # If someone intentionally stops the miner, it'll safely terminate operations.
         except KeyboardInterrupt:
             axon.stop()
-            DB.close()
             bt.logging.success('Miner killed by keyboard interrupt.')
             break
-        # In case of unforeseen errors, the miner will log the error and continue operations.
-        except Exception as e:
-            bt.logging.error(traceback.format_exc())
-            continue
 
-
-# This is the main function, which runs the miner.
 if __name__ == "__main__":
-    main( get_config() )
+    main(get_config())
--- neurons/validator.py
+++ neurons/validator.py
@@ -1,61 +1,21 @@
-# The MIT License (MIT)
-# Copyright © 2023 Yuma Rao
-# TODO(developer): Set your name
-# Copyright © 2023 <your name>
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
-# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
-# the Software.
-
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-
-# Bittensor Validator Template:
-# TODO(developer): Rewrite based on protocol defintion.
-
-# Step 1: Import necessary libraries and modules
 import os
 import time
 import torch
 import random
+import hashlib
 import argparse
-import traceback
-import bittensor as bt
-
-# Custom modules
 import rocksdb
+import bittensor as bt
+from concurrent.futures import ThreadPoolExecutor
 
-# import this repo
-import storage
-
-# Step 2: Set up the configuration parser
-# This function is responsible for setting up and parsing command-line arguments.
 def get_config():
-
     parser = argparse.ArgumentParser()
-    # TODO(developer): Adds your custom validator arguments to the parser.
-    parser.add_argument('--validator_db', default='~/validator_db', help='validator DB cache location.')
-    # Adds override arguments for network and netuid.
-    parser.add_argument( '--netuid', type = int, default = 1, help = "The chain subnet uid." )
-    # Adds subtensor specific arguments i.e. --subtensor.chain_endpoint ... --subtensor.network ...
+    parser.add_argument('--validator_db', default='~/validator_db', help='Validator DB cache location.')
+    parser.add_argument('--netuid', type=int, default=1, help="The chain subnet uid.")
     bt.subtensor.add_args(parser)
-    # Adds logging specific arguments i.e. --logging.debug ..., --logging.trace .. or --logging.logging_dir ...
     bt.logging.add_args(parser)
-    # Adds wallet specific arguments i.e. --wallet.name ..., --wallet.hotkey ./. or --wallet.path ...
     bt.wallet.add_args(parser)
-    # Parse the config (will take command-line arguments if provided)
-    # To print help message, run python3 template/miner.py --help
-    config =  bt.config(parser)
-
-    # Step 3: Set up logging directory
-    # Logging is crucial for monitoring and debugging purposes.
+    config = bt.config(parser)
     config.full_path = os.path.expanduser(
         "{}/{}/{}/netuid{}/{}".format(
             config.logging.logging_dir,
@@ -65,135 +25,76 @@ def get_config():
             'validator',
         )
     )
-    # Ensure the logging directory exists.
     if not os.path.exists(config.full_path): os.makedirs(config.full_path, exist_ok=True)
-
-    # Return the parsed config.
     return config
 
-def main( config ):
-    # Set up logging with the provided configuration and directory.
+def validate_retrieval(validation_key, validation_data_hash, dendrite, metagraph):
+    retrieve_responses = dendrite.query(
+        metagraph.axons,
+        storage.protocol.Retrieve(key=validation_key),
+        deserialize=True,
+    )
+    score = 0
+    computed_hash = hashlib.sha256(retrieve_responses.encode()).hexdigest()
+    if computed_hash == validation_data_hash.decode():
+        score = 1
+    return score
+
+def main(config):
     bt.logging(config=config, logging_dir=config.full_path)
     bt.logging.info(f"Running validator for subnet: {config.netuid} on network: {config.subtensor.chain_endpoint} with config:")
-    # Log the configuration for reference.
     bt.logging.info(config)
 
-    # Step 4: Build Bittensor validator objects
-    # These are core Bittensor classes to interact with the network.
-    bt.logging.info("Setting up bittensor objects.")
+    wallet = bt.wallet(config=config)
+    subtensor = bt.subtensor(config=config)
+    dendrite = bt.dendrite(wallet=wallet)
+    metagraph = subtensor.metagraph(config.netuid)
+    hashes_db = rocksdb.DB(config.validator_db, rocksdb.Options(create_if_missing=True))
 
-    # The wallet holds the cryptographic key pairs for the validator.
-    wallet = bt.wallet( config = config )
-    bt.logging.info(f"Wallet: {wallet}")
-
-    # The subtensor is our connection to the Bittensor blockchain.
-    subtensor = bt.subtensor( config = config )
-    bt.logging.info(f"Subtensor: {subtensor}")
-
-    # Dendrite is the RPC client; it lets us send messages to other nodes (axons) in the network.
-    dendrite = bt.dendrite( wallet = wallet )
-    bt.logging.info(f"Dendrite: {dendrite}")
-
-    # The metagraph holds the state of the network, letting us know about other miners.
-    metagraph = subtensor.metagraph( config.netuid )
-    bt.logging.info(f"Metagraph: {metagraph}")
-
-    # Build my shelve DB
-    hashes_db = rocksdb.DB(hashes_path, rocksdb.Options(create_if_missing=True))
-
-    # Step 5: Connect the validator to the network
     if wallet.hotkey.ss58_address not in metagraph.hotkeys:
-        bt.logging.error(f"\nYour validator: {wallet} if not registered to chain connection: {subtensor} \nRun btcli register and try again.")
+        bt.logging.error(f"Your validator is not registered. Run btcli register and try again.")
         exit()
     else:
-        # Each miner gets a unique identity (UID) in the network for differentiation.
         my_subnet_uid = metagraph.hotkeys.index(wallet.hotkey.ss58_address)
         bt.logging.info(f"Running validator on uid: {my_subnet_uid}")
 
-    # Step 6: Set up initial scoring weights for validation
     bt.logging.info("Building validation weights.")
     alpha = 0.9
     scores = torch.ones_like(metagraph.S, dtype=torch.float32)
-    bt.logging.info(f"Weights: {scores}")
 
-    # Step 7: The Main Validation Loop
     bt.logging.info("Starting validator loop.")
     step = 0
-    next_key = 0
-    while True:
-        try:
+    with ThreadPoolExecutor() as executor:
+        while True:
+            try:
+                validation_keys = [str(random.randint(0, 10000)) for _ in range(10)]
+                validation_data_hashes = [hashes_db.get(rocksdb.ReadOptions(), key.encode()) for key in validation_keys]
+                future_scores = [executor.submit(validate_retrieval, key, hash_val, dendrite, metagraph) for key, hash_val in zip(validation_keys, validation_data_hashes)]
 
-            # Get a random key up to the miners storage limit
-            validation_key = str( random.randint( 10000 ) )
-            validation_data_hash = hashes_db.get(rocksdb.ReadOptions(), validation_key )
-            retrieve_responses = dendrite.query(
-                # Send the query to all axons in the network.
-                metagraph.axons,
-                # Construct a dummy query.
-                storage.protocol.Retrieve( key = validation_key ), # Construct a dummy query.
-                # All responses have the deserialize function called on them before returning.
-                deserialize = True, 
-            )
+                for i, future_score in enumerate(future_scores):
+                    score = future_score.result()
+                    scores[i] = alpha * scores[i] + (1 - alpha) * score
 
-            # Log the results for monitoring purposes.
-            bt.logging.info(f"Received dummy responses: {retrieve_responses}")
+                if (step + 1) % 2 == 0:
+                    weights = torch.nn.functional.normalize(scores, p=1.0, dim=0)
+                    bt.logging.info(f"Setting weights: {weights}")
+                    result = subtensor.set_weights(
+                        netuid=config.netuid,
+                        wallet=wallet,
+                        uids=metagraph.uids,
+                        weights=weights,
+                        wait_for_inclusion=True
+                    )
+                    if result: bt.logging.success('Successfully set weights.')
+                    else: bt.logging.error('Failed to set weights.')
 
-            # TODO(developer): Define how the validator scores responses.
-            # Adjust the scores based on responses from miners.
-            for i, resp_i in enumerate(retrieve_responses):
-                # Initialize the score for the current miner's response.
-                score = 0
+                step += 1
+                metagraph = subtensor.metagraph(config.netuid)
+                time.sleep(bt.__blocktime__)
 
-                # Get the hash of the returned data and check it against the known hash.
-                computed_hash = hashlib.sha256(resp_i.encode()).hexdigest()
+            except KeyboardInterrupt:
+                bt.logging.success("Keyboard interrupt detected. Exiting validator.")
+                exit()
 
-                # Check if the miner has provided the correct response by doubling the dummy input.
-                # If correct, set their score for this round to 1.
-                if computed_hash == validation_data_hash:
-                    score = 1
-
-                # Update the global score of the miner.
-                # This score contributes to the miner's weight in the network.
-                # A higher weight means that the miner has been consistently responding correctly.
-                scores[i] = alpha * scores[i] + (1 - alpha) * score
-
-            # Periodically update the weights on the Bittensor blockchain.
-            if (step + 1) % 2 == 0:
-                # TODO(developer): Define how the validator normalizes scores before setting weights.
-                weights = torch.nn.functional.normalize(scores, p=1.0, dim=0)
-                bt.logging.info(f"Setting weights: {weights}")
-                # This is a crucial step that updates the incentive mechanism on the Bittensor blockchain.
-                # Miners with higher scores (or weights) receive a larger share of TAO rewards on this subnet.
-                result = subtensor.set_weights(
-                    netuid = config.netuid, # Subnet to set weights on.
-                    wallet = wallet, # Wallet to sign set weights using hotkey.
-                    uids = metagraph.uids, # Uids of the miners to set weights for.
-                    weights = weights, # Weights to set for the miners.
-                    wait_for_inclusion = True
-                )
-                if result: bt.logging.success('Successfully set weights.')
-                else: bt.logging.error('Failed to set weights.') 
-
-            # End the current step and prepare for the next iteration.
-            step += 1
-            # Resync our local state with the latest state from the blockchain.
-            metagraph = subtensor.metagraph(config.netuid)
-            # Sleep for a duration equivalent to the block time (i.e., time between successive blocks).
-            time.sleep(bt.__blocktime__)
-
-        # If we encounter an unexpected error, log it for debugging.
-        except RuntimeError as e:
-            bt.logging.error(e)
-            traceback.print_exc()
-
-        # If the user interrupts the program, gracefully exit.
-        except KeyboardInterrupt:
-            bt.logging.success("Keyboard interrupt detected. Exiting validator.")
-            exit()
-
-# The main function parses the configuration and runs the validator.
 if __name__ == "__main__":
-    # Parse the configuration.
-    config = get_config()
-    # Run the main function.
-    main( config )
+    main(get_config())
